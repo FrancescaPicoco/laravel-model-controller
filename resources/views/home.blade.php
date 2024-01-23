1052896b947840f9828cb05d4e4e807e992ab9b9
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-23 09:39:52 +0100

    layout ok . database mio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Films:</h1>
-<div class="cardContainer d-flex flex-wrap">
+<h1 class="text-center">Films:</h1>
+<div class="cardContainer d-flex flex-wrap container-sm m-auto">
     @foreach ($movies as $film)
     <div class="card">
-            <h3>{{$film->name}}</h3>
         <img src="{{$film->img}}" class="card-img-top">
+        <h3>{{$film->name}}</h3>
         <div class="card-body">
           <p class="card-text">{{$film->description}}</p>
         </div>
     </div>
     @endforeach
-</div>
-    
+</div>   
 @endsection
