7187de4411e3ef94e2b29528c35507c1ac0d4c4e
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-23 09:20:17 +0100

    prime card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+<h1>Films:</h1>
+<div class="cardContainer d-flex flex-wrap">
+    @foreach ($movies as $film)
+    <div class="card">
+            <h3>{{$film->name}}</h3>
+        <img src="{{$film->img}}" class="card-img-top">
+        <div class="card-body">
+          <p class="card-text">{{$film->description}}</p>
+        </div>
+    </div>
+    @endforeach
+</div>
+    
 @endsection
